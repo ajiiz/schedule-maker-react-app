4375198b121dee2e52ddfd58eb595ecff1c61d5d
ajiiz <opticpiter@gmail.com>
2020-07-19 21:42:07 +0200

    Updates

---
+++ precache-manifest.c0ac59eda6a2e20d0cb527d1a16c2b07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1b07ce63570b1d907384c7ce6231122",
+    "url": "/schedule-react-app/index.html"
+  },
+  {
+    "revision": "96e2e8d4a2a98574b404",
+    "url": "/schedule-react-app/static/css/main.8be2dc25.chunk.css"
+  },
+  {
+    "revision": "0fb73fa74d089f8d70c8",
+    "url": "/schedule-react-app/static/js/2.b6b8a8af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/schedule-react-app/static/js/2.b6b8a8af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96e2e8d4a2a98574b404",
+    "url": "/schedule-react-app/static/js/main.b2aba30f.chunk.js"
+  },
+  {
+    "revision": "f445db5a5ef5ca6e0a8b",
+    "url": "/schedule-react-app/static/js/runtime-main.3390d1aa.js"
+  }
+]);
